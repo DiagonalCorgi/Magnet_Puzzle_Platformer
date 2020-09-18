433bbad6f40ea153abf1ab709333653abe552f97
awo63 <awo63@uclive.ac.nz>
2020-09-18 17:07:12 +1200

    Setting up two-player movement, walking and jumping

--- Puzzle Platformer/Assets/Scripts/PlatformerMovement.cs
+++ Puzzle Platformer/Assets/Scripts/PlatformerMovement.cs
@@ -5,44 +5,90 @@ using UnityEngine;
 public class PlatformerMovement : MonoBehaviour
 {
 
-    float movementSpeed = 10f;
+    float movementSpeed = 20f;
     float jumpHeight = 5f;
     LayerMask layerMask = 8;
     Rigidbody rigidbody;
+    bool player1;
+    bool player2;
+    bool grounded;
 
     // Start is called before the first frame update
     void Start()
     {
         layerMask = ~layerMask;
         rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (gameObject.tag == "Player1")
+        {
+            player1 = true;
+            Debug.Log("Player1 found");
+        }
+        else if (gameObject.tag == "Player2")
+        {
+            player2 = true;
+            Debug.Log("Player2 found");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetButton("Horizontal"))
+
+        if (player1)
         {
-            Debug.Log("moving left/right");
-            if (Input.GetAxis("Horizontal") > 0f)
-            {
-                transform.Translate(new Vector3(movementSpeed * Time.deltaTime, 0f, 0f), Space.Self);
-            }
-            else if (Input.GetAxis("Horizontal") < 0f)
-            {
-                transform.Translate(new Vector3(-movementSpeed * Time.deltaTime, 0f, 0f), Space.Self);
-            }
+            MovePlayer(1);
+        }
+        else if (player2)
+        {
+            MovePlayer(2);
         }
 
-        if (Input.GetButtonDown("Vertical"))
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.55f, layerMask))
         {
-            if (Input.GetAxis("Vertical") > 0f)
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+
+        }
+
+    public void MovePlayer(int player)
+    {
+        if (Input.GetButton("HorizontalPlayer" + player))
+        {
+            if (Input.GetAxis("HorizontalPlayer" + player) > 0f)
             {
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.55f, layerMask))
+                if (rigidbody.velocity.x < 6f)
                 {
-                    Debug.Log("jump");
+                    rigidbody.AddForce(new Vector3(movementSpeed, 0f, 0f), ForceMode.Acceleration);
+                }
+            }
+            else if (Input.GetAxis("HorizontalPlayer" + player) < 0f)
+            {
+                if (rigidbody.velocity.x < 6f)
+                {
+                    rigidbody.AddForce(new Vector3(-movementSpeed, 0f, 0f), ForceMode.Acceleration);
+                }
+            }
+        }
+        if (!Input.GetButton("HorizontalPlayer" + player) && grounded)
+        {
+            rigidbody.velocity = rigidbody.velocity * 0.9f;
+        }
+
+        if (Input.GetButtonDown("VerticalPlayer" + player))
+        {
+            if (Input.GetAxis("VerticalPlayer" + player) > 0f)
+            {
+                
+                if (grounded)
+                {
                     rigidbody.AddForce(transform.TransformDirection(Vector3.up * jumpHeight), ForceMode.Impulse);
-                    
+
                 }
             }
         }
