04cd28cb32a7279a8da1788a5c6ba836d93544a6
awo63 <awo63@uclive.ac.nz>
2020-09-23 11:58:13 +1200

    Setting up button prefab with animations

---
+++ Puzzle Platformer/Assets/Scripts/Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    bool beingPressed;
+    GameObject[] objectsPressing;
+    Animator buttonAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       buttonAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        beingPressed = true;
+        Debug.Log(beingPressed);
+        buttonAnimator.SetBool("IsDown", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        beingPressed = false;
+        Debug.Log(beingPressed);
+        buttonAnimator.SetBool("IsDown", false);
+    }
+}
