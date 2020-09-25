22c83149bfac4fc485e63e7c8df081db7c6ec7aa
dca87 <dca87@uclive.ac.nz>
2020-09-25 13:13:16 +1200

    added in keybinds difference for polarity changes and colour changes for each player to see the changes easily

--- Puzzle Platformer/Assets/Scripts/PlayerMagnet.cs
+++ Puzzle Platformer/Assets/Scripts/PlayerMagnet.cs
@@ -4,50 +4,67 @@ using UnityEngine;
 
 public class PlayerMagnet : MonoBehaviour
 {
+
+
     public Magnet magnet;
     // Start is called before the first frame update
     void Start()
     {
+
+
         magnet = GetComponentInChildren<Magnet>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
+        var player1Renderer = player1.GetComponent<Renderer>();
 
-     if(Input.GetButtonDown("ToggleMagnetPlayer1"))
+        GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
+        var player2Renderer = player2.GetComponent<Renderer>();
+
+        if (Input.GetButtonDown("NorthPolarityPlayer1"))
         {
-            Debug.Log("toggle");
+            player1Renderer.material.SetColor("_Color", Color.blue);
+            magnet.MagnetForce = 10;
+            magnet.MagneticPole = Magnet.Pole.North;
+            Debug.Log("North");
+        }
+        if (Input.GetButtonDown("SouthPolarityPlayer1"))
+        {
+            player1Renderer.material.SetColor("_Color", Color.red);
+            magnet.MagnetForce = 10;
+            magnet.MagneticPole = Magnet.Pole.South;
+            Debug.Log("South");
+        }
+        if (Input.GetButtonDown("NeutralPolarityPlayer1"))
+        {
+            player1Renderer.material.SetColor("_Color", Color.grey);
+            magnet.MagnetForce = 0;
+            Debug.Log("Neutral");
+        }
+        if (Input.GetButtonDown("NorthPolarityPlayer2"))
+        {
+            player2Renderer.material.SetColor("_Color", Color.blue);
+            magnet.MagnetForce = 10;
+            magnet.MagneticPole = Magnet.Pole.North;
+            Debug.Log("North");
+        }
+        if (Input.GetButtonDown("SouthPolarityPlayer2"))
+        {
+            player2Renderer.material.SetColor("_Color", Color.red);
+            magnet.MagnetForce = 10;
+            magnet.MagneticPole = Magnet.Pole.South;
+            Debug.Log("South");
+        }
+        if (Input.GetButtonDown("NeutralPolarityPlayer2"))
+        {
+            player2Renderer.material.SetColor("_Color", Color.grey);
+            magnet.MagnetForce = 0;
+            Debug.Log("Neutral");
         }
 
-        if (Input.GetButtonDown("SwitchPolarityPlayer1"))
-        {
-            if (magnet.MagneticPole == Magnet.Pole.North)
-            {
-                magnet.MagneticPole = Magnet.Pole.South;
-            }
-            else
-            {
-                magnet.MagneticPole = Magnet.Pole.North;
-            }
-            Debug.Log(magnet.MagneticPole);
-        }
-     if(Input.GetButtonDown("ToggleMagnetPlayer2"))
-        {
-            Debug.Log("toggle");
-        }
 
-        if (Input.GetButtonDown("SwitchPolarityPlayer2"))
-        {
-            if (magnet.MagneticPole == Magnet.Pole.North)
-            {
-                magnet.MagneticPole = Magnet.Pole.South;
-            }
-            else
-            {
-                magnet.MagneticPole = Magnet.Pole.North;
-            }
-            Debug.Log(magnet.MagneticPole);
-        }
     }
 }
