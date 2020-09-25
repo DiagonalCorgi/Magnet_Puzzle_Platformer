7d99f95c99c0699429e717ee69eff2c35ee2048e
dca87 <dca87@uclive.ac.nz>
2020-09-25 13:44:46 +1200

    fixed issue with magnet polarity not changing to specific tag

--- Puzzle Platformer/Assets/Scripts/PlayerMagnet.cs
+++ Puzzle Platformer/Assets/Scripts/PlayerMagnet.cs
@@ -7,11 +7,12 @@ public class PlayerMagnet : MonoBehaviour
 
 
     public Magnet magnet;
+    public string playerTag;
     // Start is called before the first frame update
     void Start()
     {
 
-
+        playerTag = gameObject.tag;
         magnet = GetComponentInChildren<Magnet>();
     }
 
@@ -24,41 +25,41 @@ public class PlayerMagnet : MonoBehaviour
         GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
         var player2Renderer = player2.GetComponent<Renderer>();
 
-        if (Input.GetButtonDown("NorthPolarityPlayer1"))
+        if (Input.GetButtonDown("NorthPolarityPlayer1") && playerTag == "Player1")
         {
             player1Renderer.material.SetColor("_Color", Color.blue);
-            magnet.MagnetForce = 10;
+            magnet.MagnetForce = 15;
             magnet.MagneticPole = Magnet.Pole.North;
             Debug.Log("North");
         }
-        if (Input.GetButtonDown("SouthPolarityPlayer1"))
+        if (Input.GetButtonDown("SouthPolarityPlayer1") && playerTag == "Player1")
         {
             player1Renderer.material.SetColor("_Color", Color.red);
-            magnet.MagnetForce = 10;
+            magnet.MagnetForce = 15;
             magnet.MagneticPole = Magnet.Pole.South;
             Debug.Log("South");
         }
-        if (Input.GetButtonDown("NeutralPolarityPlayer1"))
+        if (Input.GetButtonDown("NeutralPolarityPlayer1") && playerTag == "Player1")
         {
             player1Renderer.material.SetColor("_Color", Color.grey);
             magnet.MagnetForce = 0;
             Debug.Log("Neutral");
         }
-        if (Input.GetButtonDown("NorthPolarityPlayer2"))
+        if (Input.GetButtonDown("NorthPolarityPlayer2") && playerTag == "Player2")
         {
             player2Renderer.material.SetColor("_Color", Color.blue);
-            magnet.MagnetForce = 10;
+            magnet.MagnetForce = 15;
             magnet.MagneticPole = Magnet.Pole.North;
             Debug.Log("North");
         }
-        if (Input.GetButtonDown("SouthPolarityPlayer2"))
+        if (Input.GetButtonDown("SouthPolarityPlayer2") && playerTag == "Player2")
         {
             player2Renderer.material.SetColor("_Color", Color.red);
-            magnet.MagnetForce = 10;
+            magnet.MagnetForce = 15;
             magnet.MagneticPole = Magnet.Pole.South;
             Debug.Log("South");
         }
-        if (Input.GetButtonDown("NeutralPolarityPlayer2"))
+        if (Input.GetButtonDown("NeutralPolarityPlayer2") && playerTag == "Player2")
         {
             player2Renderer.material.SetColor("_Color", Color.grey);
             magnet.MagnetForce = 0;
