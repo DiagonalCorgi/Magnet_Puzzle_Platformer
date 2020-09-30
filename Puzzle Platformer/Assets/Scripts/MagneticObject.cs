a9cd82b1ca992aeb878f6cc8ade02904413fd2aa
dca87 <dca87@uclive.ac.nz>
2020-09-30 13:50:05 +1300

    Added object_magnet script just need to make sure it works properly with colliders

---
+++ Puzzle Platformer/Assets/Scripts/MagneticObject.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void SetKinematic()
+    {
+        rb.isKinematic = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+        Vector3 collisionDirection;
+        float distance;
+        Physics.ComputePenetration(collision.other.GetComponent<Collider>(), collision.other.transform.position +Vector3.down*1f, collision.other.transform.rotation, gameObject.GetComponent<Collider>(), transform.position, transform.rotation, out collisionDirection, out distance);  
+
+
+
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            if (distance > 0.01f)
+            {
+                rb.isKinematic = false;
+            }
+            else
+            {
+                rb.isKinematic = true;
+            }
+            
+            
+        }   
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+        Vector3 collisionDirection;
+        float distance;
+        Physics.ComputePenetration(collision.other.GetComponent<Collider>(), collision.other.transform.position + Vector3.down * 0.5f, collision.other.transform.rotation, gameObject.GetComponent<Collider>(), transform.position, transform.rotation, out collisionDirection, out distance);
+
+
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            if (distance > 0.01f)
+            {
+                rb.isKinematic = true;
+                Invoke("SetKinematic", 2.0f);
+            }
+            else
+            {
+                rb.isKinematic = false;
+            }
+        }
+    }
+}
